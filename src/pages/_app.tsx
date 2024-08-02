b38b073297107477503e60fc377d54e9172abed9
Junho Yeo <i@junho.io>
2024-08-02 22:55:00 +0900

    Setup `@aptos-labs/wallet-adapter-react`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,34 @@
+import { Network } from '@aptos-labs/ts-sdk';
+import { AptosWalletAdapterProvider } from '@aptos-labs/wallet-adapter-react';
 import { AppProps } from 'next/app';
 import React from 'react';
 
 import { SatoshiFont } from '@/styles/fonts';
 import '@/styles/global.css';
 
+// const wallets = [
+//   new BitgetWallet(),
+//   new FewchaWallet(),
+//   new MartianWallet(),
+//   new MSafeWalletAdapter(),
+//   new PontemWallet(),
+//   new TrustWallet(),
+//   new OKXWallet(),
+// ];
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <React.Fragment>
-      <Component {...pageProps} />
+      <AptosWalletAdapterProvider
+        // plugins={wallets}
+        autoConnect={true}
+        dappConfig={{ network: Network.MAINNET }}
+        onError={(error) => {
+          console.log('error', error);
+        }}
+      >
+        <Component {...pageProps} />
+      </AptosWalletAdapterProvider>
 
       <div id="portal" />
 
