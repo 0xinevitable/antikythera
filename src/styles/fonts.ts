fcec732eeba711abe36f6aaa34aa928b23f0a2e8
Junho Yeo <i@junho.io>
2024-08-16 17:11:37 +0900

    Use woff2

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,7 +1,7 @@
 import localFont from 'next/font/local';
 
 export const SatoshiFont = localFont({
-  src: '../../public/fonts/Satoshi-Variable.ttf',
+  src: '../../public/fonts/Satoshi-Variable.woff2',
   variable: '--satoshi-variable',
   fallback: ['sans-serif'],
 });
